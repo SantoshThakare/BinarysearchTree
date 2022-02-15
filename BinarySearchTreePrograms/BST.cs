8b5d78d2ddfa07be06a730be9225ffb6b0a9ab54
SantoshThakare <santoshthakare50@gmail.com>
2022-02-15 14:40:18 +0530

    Add Element

---
+++ BinarySearchTreePrograms/BST.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTreePrograms
+{
+    public class BST<T>where T : IComparable<T>
+    {
+        public T nodeData { get; set; }
+        public BST<T> LeftTree { get; set; }
+        public BST<T> RightTree { get; set; }
+        public BST(T Data)
+        {
+            this.nodeData = Data;
+            this.LeftTree = null;
+            this.RightTree = null;
+        }
+        int leftCount = 0;
+        int rightCount = 0;
+        private bool result;
+        public void Insert(T Item)
+        {
+            T CurrNodeVal = this.nodeData;
+            if ((CurrNodeVal.CompareTo(Item))>0)
+            {
+                if (this.LeftTree == null)
+                {
+                    this.LeftTree = new BST<T>(Item);
+                }
+                else
+                {
+                    this.LeftTree.Insert(Item);
+                }
+            }
+            else
+            {
+                if (this.RightTree == null)
+                {
+                    this.RightTree =new BST<T>(Item);   
+                }
+                else
+                {
+                    this.RightTree.Insert(Item);
+                }
+            }
+        }
+        public void Display()
+        {
+            if (this.LeftTree != null)
+            {
+                this.leftCount++;
+                this.LeftTree.Display();
+            }
+            Console.WriteLine(this.nodeData.ToString());
+            
+               if (this.RightTree != null)
+            {
+                this.rightCount++;
+                this.RightTree.Display();
+            }
+        }
+    }
+}
---
+++ BinarySearchTreePrograms/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTreePrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Binary Tree Search Program ");
+            Console.WriteLine("\n 1. Insertion");
+            Console.WriteLine("Enter the the 1");
+            int num = Convert.ToInt32(Console.ReadLine());
+            BST<int> BinarySearchTree = new BST<int>(20);
+            switch (num)
+            {
+                case 1:
+                    BinarySearchTree.Insert(30);
+                    BinarySearchTree.Insert(40);
+                    BinarySearchTree.Display();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
